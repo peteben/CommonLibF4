bf9015c96942493b3464ee329dc75d51d7c040ca
Peteben <peteben@gmail.com>
2026-01-03 18:36:41 -0600

    Fixed broken merge, added comments

--- CommonLibF4/include/RE/Bethesda/FunctionArgs.h
+++ CommonLibF4/include/RE/Bethesda/FunctionArgs.h
@@ -24,34 +24,29 @@ SOFTWARE.
 
 #pragma once
 
-namespace Papyrus
-{
+namespace Papyrus {
 	template <class F>
 	using BSTThreadScrapFunctionOG = RE::msvc::function<F>;  // Used by OG/VR for DispatchMethod calls
 
-	namespace detail
-	{
-		template <class... Args>
-		RE::BSScrapArray<RE::BSScript::Variable> PackVariables(Args&&... a_args)
-		{
-			constexpr auto                           size = sizeof...(a_args);
-			auto                                     args = std::make_tuple(std::forward<Args>(a_args)...);
-			RE::BSScrapArray<RE::BSScript::Variable> result{ size };
-			[&]<std::size_t... p>(std::index_sequence<p...>) {
-				((RE::BSScript::PackVariable(result.at(p), std::get<p>(args))), ...);
-			}(std::make_index_sequence<size>{});
-			return result;
-		}
+	namespace detail {
+        template <class... Args>
+        RE::BSScrapArray<RE::BSScript::Variable> PackVariables(Args&&... a_args) {
+            constexpr auto size = sizeof...(a_args);
+            auto args = std::make_tuple(std::forward<Args>(a_args)...);
+            RE::BSScrapArray<RE::BSScript::Variable> result{size};
+            [&]<std::size_t... p>(std::index_sequence<p...>) {
+                ((RE::BSScript::PackVariable(result.at(p), std::get<p>(args))), ...);
+            }(std::make_index_sequence<size>{});
+            return result;
+			}
 
-		class FunctionArgsBase
-		{
-		public:
-			FunctionArgsBase() = delete;
+        class FunctionArgsBase {
+        public:
+            FunctionArgsBase() = delete;
 
-			FunctionArgsBase(RE::BSScript::IVirtualMachine* a_vm) :
-				args(nullptr), vm(a_vm) {}
+            FunctionArgsBase(RE::BSScript::IVirtualMachine* a_vm) : args(nullptr),vm(a_vm) {}
 
-			/*
+            /*
             bool operator()(RE::BSScrapArray<RE::BSScript::Variable>& a_args)
             {
                 args->GetArrayCopy(a_args, *vm);
@@ -59,41 +54,36 @@ namespace Papyrus
             }
             */
 
-		protected:
-			// members
-			RE::BSScript::ArrayWrapper<RE::BSScript::Variable>* args;  // 00
-			RE::BSScript::IVirtualMachine*                      vm;    // 08
-		};
+        protected:
+            // members
+            RE::BSScript::ArrayWrapper<RE::BSScript::Variable>* args;  // 00
+            RE::BSScript::IVirtualMachine* vm;                         // 08
+        };
 
-		static_assert(sizeof(FunctionArgsBase) == 0x10);
+        static_assert(sizeof(FunctionArgsBase) == 0x10);
 
-		inline BSTThreadScrapFunctionOG<bool(RE::BSScrapArray<RE::BSScript::Variable>&)> CreateThreadScrapFunction(
-			FunctionArgsBase& a_args)
-		{
-			using func_t = decltype(&detail::CreateThreadScrapFunction);
-			REL::Relocation<func_t> func{ REL::ID(69733) };
-			return func(a_args);
-		}
-	}
+        inline BSTThreadScrapFunctionOG<bool(RE::BSScrapArray<RE::BSScript::Variable>&)> CreateThreadScrapFunction(
+            FunctionArgsBase& a_args) {
+            using func_t = decltype(&detail::CreateThreadScrapFunction);
+            REL::Relocation<func_t> func{REL::ID(69733)};
+            return func(a_args);
+        }
+    }
 
-	template <class... Args>
-	class FunctionArgs : public detail::FunctionArgsBase
-	{
-	public:
-		FunctionArgs() = delete;
+    template <class... Args>
+    class FunctionArgs : public detail::FunctionArgsBase {
+    public:
+        FunctionArgs() = delete;
 
-		FunctionArgs(RE::BSScript::IVirtualMachine* a_vm, Args... a_args) :
-			FunctionArgsBase(a_vm)
-		{
-			auto scrap = detail::PackVariables(a_args...);
-			args = new RE::BSScript::ArrayWrapper<RE::BSScript::Variable>(scrap, *vm);
-		}
+        FunctionArgs(RE::BSScript::IVirtualMachine* a_vm, Args... a_args) : FunctionArgsBase(a_vm) {
+            auto scrap = detail::PackVariables(a_args...);
+            args = new RE::BSScript::ArrayWrapper<RE::BSScript::Variable>(scrap, *vm);
+        }
 
-		BSTThreadScrapFunctionOG<bool(RE::BSScrapArray<RE::BSScript::Variable>&)> get()
-		{
-			return detail::CreateThreadScrapFunction(*this);
-		}
-	};
+        BSTThreadScrapFunctionOG<bool(RE::BSScrapArray<RE::BSScript::Variable>&)> get() {
+            return detail::CreateThreadScrapFunction(*this);
+        }
+    };
 
-	static_assert(sizeof(FunctionArgs<std::monostate>) == 0x10);
+    static_assert(sizeof(FunctionArgs<std::monostate>) == 0x10);
 }
--- CommonLibF4/include/RE/Bethesda/SendPapyrusEvent.h
+++ CommonLibF4/include/RE/Bethesda/SendPapyrusEvent.h
@@ -1,6 +1,6 @@
 #include "PCH.h"
-#include <RE/Bethesda/FunctionArgs.h>
 #include <RE/msvc/functional.h>
+#include <RE/Bethesda/FunctionArgs.h>
 
 /*
 Functions for calling Papyrus functions from C++
@@ -11,239 +11,163 @@ Usage: in Papyrus, use 'RegisterForExternalEvent'
 in C++, call 'SendPapyrusExternalEvent'
 */
 
-namespace Papyrus
-{
-	namespace detail
-	{
 
-		template <class F>
-		using BSTThreadScrapFunction = std::function<F>;  // Used by NG for DispatchMethod calls
+namespace Papyrus {
+namespace detail {
 
-		template <class F>
-		using BSTThreadScrapFunctionOG = RE::msvc::function<F>;  // Used by OG for same
+template <class F>
+using BSTThreadScrapFunction = std::function<F>;  // Used by NG for DispatchMethod calls
 
-		size_t GetVtableFunctionAddr(int entry)
-		{
-			auto    VMvtbl = RE::BSScript::Internal::VirtualMachine::VTABLE;
-			REL::ID VMvtblID = VMvtbl[0];
+template <class F>
+using BSTThreadScrapFunctionOG = RE::msvc::function<F>;  // Used by OG for same
 
-			void** pt = (void**)VMvtblID.address();
-			return (size_t)pt[entry];
+// Get function address from VirtualMachine Vtable
+size_t GetVtableFunctionAddr(int entry) {
+	auto    VMvtbl = RE::BSScript::Internal::VirtualMachine::VTABLE;
+	REL::ID VMvtblID = VMvtbl[0];
+
+	void** pt = (void**)VMvtblID.address();
+	return (size_t)pt[entry];
+	}
+
+// NG can use the current IVirtualMachine::DispatchMethodCall as is.
+// OG/VR, we need to call the function with different args.
+
+bool DispatchMethodCallOG(
+	RE::BSScript::IVirtualMachine* vm,
+	std::uint64_t a_objHandle,
+	const RE::BSFixedString& a_objName,
+	const RE::BSFixedString& a_funcName,
+	const BSTThreadScrapFunctionOG<bool(RE::BSScrapArray<RE::BSScript::Variable>&)>& a_arguments,
+	const RE::BSTSmartPointer<RE::BSScript::IStackCallbackFunctor>& a_callback) {
+
+	// Get function address out of the Vtable
+	using func_t = decltype(&DispatchMethodCallOG);
+	REL::Relocation<func_t> func{ GetVtableFunctionAddr(46) };
+
+	return func(vm, a_objHandle, a_objName, a_funcName, a_arguments, a_callback);
+	}
+
+// OG/VR version of DispatchStatic call
+
+bool DispatchStaticCallOG(
+	RE::BSScript::IVirtualMachine* vm,
+	const RE::BSFixedString& a_objName,
+	const RE::BSFixedString& a_funcName,
+	const BSTThreadScrapFunctionOG<bool(RE::BSScrapArray<RE::BSScript::Variable>&)>& a_arguments,
+	const RE::BSTSmartPointer<RE::BSScript::IStackCallbackFunctor>& a_callback) {
+
+	// Get function address out of the Vtable
+	using func_t = decltype(&DispatchStaticCallOG);
+	REL::Relocation<func_t> func{ GetVtableFunctionAddr(44) };
+
+	return func(vm, a_objName, a_funcName, a_arguments, a_callback);
+	}
+
+
+// Generic DispatchStaticCall used to call global Papyrus functions
+
+template <class... Args>
+bool DispatchStaticCall(
+	RE::BSScript::IVirtualMachine* vm,						// VM pointer
+	const RE::BSFixedString& a_objName,						// Papyrus script name
+	const RE::BSFixedString& a_funcName,					// Papyrus function name
+	const RE::BSTSmartPointer<RE::BSScript::IStackCallbackFunctor>& a_callback,
+	Args... a_args) {										// Args, must match Papyrus declaration
+
+	if (REL::Module::IsNG()) {
+		return vm->DispatchStaticCall(
+			a_objName,
+			a_funcName,
+			[&](RE::BSScrapArray<RE::BSScript::Variable>& a_out) {
+			a_out = detail::PackVariables(a_args...);
+			return true;
+			},
+			a_callback);
 		}
-
-		// NG can use the current IVirtualMachine::DispatchMethodCall as is.
-		// OG/VR, we need to call the function with different args.
-
-		bool DispatchMethodCallOG(
-			RE::BSScript::IVirtualMachine*                                                   vm,
-			std::uint64_t                                                                    a_objHandle,
-			const RE::BSFixedString&                                                         a_objName,
-			const RE::BSFixedString&                                                         a_funcName,
-			const BSTThreadScrapFunctionOG<bool(RE::BSScrapArray<RE::BSScript::Variable>&)>& a_arguments,
-			const RE::BSTSmartPointer<RE::BSScript::IStackCallbackFunctor>&                  a_callback)
-		{
-			// Get function address out of the Vtable
-			auto    VMvtbl = RE::BSScript::Internal::VirtualMachine::VTABLE;
-			REL::ID VMvtblID = VMvtbl[0];
-			UINT64  DispatchMethodptr;
-
-			// Get function address out of the Vtable
-			using func_t = decltype(&DispatchMethodCallOG);
-			REL::Relocation<func_t> func{ GetVtableFunctionAddr(46) };
-
-			return func(vm, a_objHandle, a_objName, a_funcName, a_arguments, a_callback);
+	else {
+		return DispatchStaticCallOG(vm, a_objName, a_funcName, (FunctionArgs{ vm, a_args... }).get(), a_callback);
 		}
+	}
 
-		bool DispatchStaticCallOG(
-			RE::BSScript::IVirtualMachine*                                                   vm,
-			const RE::BSFixedString&                                                         a_objName,
-			const RE::BSFixedString&                                                         a_funcName,
-			const BSTThreadScrapFunctionOG<bool(RE::BSScrapArray<RE::BSScript::Variable>&)>& a_arguments,
-			const RE::BSTSmartPointer<RE::BSScript::IStackCallbackFunctor>&                  a_callback)
-		{
-			// Get function address out of the Vtable
-			using func_t = decltype(&DispatchStaticCallOG);
-			REL::Relocation<func_t> func{ GetVtableFunctionAddr(44) };
+// Generic DispatchMethodCall used to call Papyrus function on a specific script instance
 
-			return func(vm, a_objName, a_funcName, a_arguments, a_callback);
+template <class... Args>
+bool DispatchMethodCall(
+	RE::BSScript::IVirtualMachine* vm,						// VM pointer
+	std::uint64_t a_objHandle,								// Handle to script instance
+	const RE::BSFixedString& a_objName,						// Papyrus Script name
+	const RE::BSFixedString& a_funcName,					// Papyrus function name
+	const RE::BSTSmartPointer<RE::BSScript::IStackCallbackFunctor>& a_callback,
+	Args... a_args) {										// Args, must match Papyrus declaration
+
+	if (REL::Module::IsNG()) {
+		return vm->DispatchMethodCall(
+			a_objHandle,
+			a_objName,
+			a_funcName,
+			[&](RE::BSScrapArray<RE::BSScript::Variable>& a_out) {
+			a_out = detail::PackVariables(a_args...);
+			return true;
+			},
+			a_callback);
 		}
+	else {
+		return DispatchMethodCallOG(vm, a_objHandle, a_objName, a_funcName, (FunctionArgs{ vm, a_args... }).get(), a_callback);
+		}
+	}
 
-		// Generic DispatchStaticCall
 
-		template <class... Args>
-		bool DispatchStaticCall(
-			RE::BSScript::IVirtualMachine*                                  vm,
-			const RE::BSFixedString&                                        a_objName,
-			const RE::BSFixedString&                                        a_funcName,
-			const RE::BSTSmartPointer<RE::BSScript::IStackCallbackFunctor>& a_callback,
-			Args... a_args)
-		{
-			if (REL::Module::IsNG()) {
-				return vm->DispatchStaticCall(
-					a_objName,
-					a_funcName,
-					[&](RE::BSScrapArray<RE::BSScript::Variable>& a_out) {
-						a_out = detail::PackVariables(a_args...);
-						return true;
-					},
-					a_callback);
-			}
+// Call all the Papyrus functions registered with a_eventName
+// Call RegisterForExternalEvent to register Papyrus functions to be called.
 
-			bool DispatchStaticCallOG(
-				RE::BSScript::IVirtualMachine * vm,
-				const RE::BSFixedString&                                                         a_objName,
-				const RE::BSFixedString&                                                         a_funcName,
-				const BSTThreadScrapFunctionOG<bool(RE::BSScrapArray<RE::BSScript::Variable>&)>& a_arguments,
-				const RE::BSTSmartPointer<RE::BSScript::IStackCallbackFunctor>&                  a_callback)
-			{
-				// Get function address out of the Vtable
-				auto    VMvtbl = RE::BSScript::Internal::VirtualMachine::VTABLE;
-				REL::ID VMvtblID = VMvtbl[0];
-				UINT64  DispatchMethodptr;
+template <class... Args>
+static void SendPapyrusExternalEvent(std::string a_eventName, Args... _args) {
+	auto const papyrus = F4SE::GetPapyrusInterface();
+	auto* vm = RE::GameVM::GetSingleton()->GetVM().get();
 
-				void** pt = (void**)VMvtblID.address();
-				DispatchMethodptr = (size_t)pt[44];
 
-				using func_t = decltype(&DispatchStaticCallOG);
-				REL::Relocation<func_t> func{ DispatchMethodptr };
+	if (REL::Module::IsNG()) {
+		struct PapyrusEventData {
+			RE::BSScript::IVirtualMachine* vm;
+			RE::BSScrapArray<RE::BSScript::Variable> argsPacked;
+			};
+		PapyrusEventData evntData;
 
-				return func(vm, a_objName, a_funcName, a_arguments, a_callback);
-			}
-
-			// Generic DispatchStaticCall
-
-			template <class... Args>
-			bool DispatchMethodCall(
-				RE::BSScript::IVirtualMachine * vm,
-				std::uint64_t                                                   a_objHandle,
-				const RE::BSFixedString&                                        a_objName,
-				const RE::BSFixedString&                                        a_funcName,
-				const RE::BSTSmartPointer<RE::BSScript::IStackCallbackFunctor>& a_callback,
-				Args... a_args)
-			{
-				if (REL::Module::IsNG()) {
-					return vm->DispatchMethodCall(
-						a_objHandle,
-						a_objName,
-						a_funcName,
-						[&](RE::BSScrapArray<RE::BSScript::Variable>& a_out) {
-							a_out = detail::PackVariables(a_args...);
-							return true;
-						},
-						a_callback);
-				} else {
-					return DispatchMethodCallOG(vm, a_objHandle, a_objName, a_funcName, (FunctionArgs{ vm, a_args... }).get(), a_callback);
-				}
-			}
-
-			// Call all the Papyrus functions registered with a_eventName
-
-			template <class... Args>
-			static void SendPapyrusExternalEvent(std::string a_eventName, Args... _args)
-			{
-				auto const papyrus = F4SE::GetPapyrusInterface();
-				auto*      vm = RE::GameVM::GetSingleton()->GetVM().get();
-
-				if (REL::Module::IsNG()) {
-					struct PapyrusEventData
-					{
-						RE::BSScript::IVirtualMachine*           vm;
-						RE::BSScrapArray<RE::BSScript::Variable> argsPacked;
-					};
-					PapyrusEventData evntData;
-
-					evntData.argsPacked = PackVariables(_args...);
-					evntData.vm = vm;
+		evntData.argsPacked = PackVariables(_args...);
+		evntData.vm = vm;
 
 		papyrus->GetExternalEventRegistrations(
 			a_eventName, &evntData,
 			[](uint64_t handle, const char* scriptName, const char* callbackName, void* dataPtr) {
-						PapyrusEventData* d = static_cast<PapyrusEventData*>(dataPtr);
-						d->vm->DispatchMethodCall(
-							handle, scriptName, callbackName,
-							[&](RE::BSScrapArray<RE::BSScript::Variable>& a_out) {
-								a_out = d->argsPacked;
-								return true;
-							},
-							nullptr);
-			}
-				}
-
-				// Generic DispatchMethodCall
-
-				template <class... Args>
-				bool DispatchMethodCall(
-					RE::BSScript::IVirtualMachine * vm,
-					std::uint64_t                                                   a_objHandle,
-					const RE::BSFixedString&                                        a_objName,
-					const RE::BSFixedString&                                        a_funcName,
-					const RE::BSTSmartPointer<RE::BSScript::IStackCallbackFunctor>& a_callback,
-					Args... a_args)
-				{
-		if (REL::Module::IsNG()) {
-			return vm->DispatchMethodCall(
-				a_objHandle,
-				a_objName,
-				a_funcName,
+			PapyrusEventData* d = static_cast<PapyrusEventData*>(dataPtr);
+			d->vm->DispatchMethodCall(handle, scriptName, callbackName,
 				[&](RE::BSScrapArray<RE::BSScript::Variable>& a_out) {
-					a_out = detail::PackVariables(a_args...);
-					return true;
+				a_out = d->argsPacked;
+				return true;
 				},
-				a_callback);
-		} else {
-			return DispatchMethodCallOG(vm, a_objHandle, a_objName, a_funcName, (FunctionArgs{ vm, a_args... }).get(), nullptr);
-		}
-				}
-
-				// Call all the Papyrus functions registered with a_eventName
-
-				template <class... Args>
-				static void SendPapyrusExternalEvent(std::string a_eventName, Args... _args)
-				{
-		auto const papyrus = F4SE::GetPapyrusInterface();
-		auto*      vm = RE::GameVM::GetSingleton()->GetVM().get();
-
-		if (REL::Module::IsNG()) {
-			struct PapyrusEventData
-			{
-				RE::BSScript::IVirtualMachine*           vm;
-				RE::BSScrapArray<RE::BSScript::Variable> argsPacked;
-			};
-			PapyrusEventData evntData;
-
-			evntData.argsPacked = PackVariables(_args...);
-			evntData.vm = vm;
-
-			papyrus->GetExternalEventRegistrations(
-				a_eventName, &evntData,
-				[](uint64_t handle, const char* scriptName, const char* callbackName, void* dataPtr) {
-					PapyrusEventData* d = (PapyrusEventData*)dataPtr;
-					d->vm->DispatchMethodCall(
-						handle, scriptName, callbackName,
-						[&](RE::BSScrapArray<RE::BSScript::Variable>& a_out) {
-							a_out = d->argsPacked;
-							return true;
-						},
-						nullptr);
-				});
-		} else {
-			struct PapyrusEventData
-			{
-				RE::BSScript::IVirtualMachine*                                            vm;
-				BSTThreadScrapFunctionOG<bool(RE::BSScrapArray<RE::BSScript::Variable>&)> scrapFunc;
-			};
-			PapyrusEventData evntData;
-
-			evntData.scrapFunc = (FunctionArgs{ vm, _args... }).get();
-			evntData.vm = vm;
-
-			papyrus->GetExternalEventRegistrations(
-				a_eventName, &evntData,
-				[](uint64_t handle, const char* scriptName, const char* callbackName, void* dataPtr) {
-					PapyrusEventData* d = static_cast<PapyrusEventData*>(dataPtr);
-					DispatchMethodCallOG(d->vm, handle, scriptName, callbackName, d->scrapFunc, nullptr);
-				});
-		}
-				}
+				nullptr);
 			}
+		);
 		}
+	else {
+		struct PapyrusEventData {
+			RE::BSScript::IVirtualMachine* vm;
+			BSTThreadScrapFunctionOG<bool(RE::BSScrapArray<RE::BSScript::Variable>&)> scrapFunc;
+			};
+		PapyrusEventData evntData;
+
+		evntData.scrapFunc = (FunctionArgs{ vm, _args... }).get();
+		evntData.vm = vm;
+
+		papyrus->GetExternalEventRegistrations(
+			a_eventName, &evntData,
+			[](uint64_t handle, const char* scriptName, const char* callbackName, void* dataPtr) {
+			PapyrusEventData* d = static_cast<PapyrusEventData*>(dataPtr);
+			DispatchMethodCallOG(d->vm, handle, scriptName, callbackName, d->scrapFunc, nullptr);
+			}
+		);
+		}
+	}
+}
+}
